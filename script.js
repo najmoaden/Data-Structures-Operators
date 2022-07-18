1b967b54da549e26467008cb798ba67a9635dfdd
najmoaden <78856004+najmoaden@users.noreply.github.com>
2022-07-18 16:45:59 +0300

    The Spread Operator (...)

--- script.js
+++ script.js
@@ -40,6 +40,11 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time},`
     );
   },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here's your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 };
 
 /* //Destructing Arrays
@@ -80,7 +85,7 @@ console.log(i, j, k); //2, 5 and 6
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
  */
-
+/* 
 //Destructing Objects
 const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
@@ -114,3 +119,50 @@ restaurant.orderDelivery({
 });
 
 restaurant.orderDelivery({ address: 'Via del Sole, 21', starterIndex: 1 });
+ */
+
+//The Spread Operator (...)
+
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+//Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+console.log(mainMenuCopy);
+//Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+//Iterables: arrays, strings, maps, sets NOT objects
+const str = 'Jonas';
+const letters = [...str, '', 'S.'];
+console.log(letters);
+console.log(...str);
+
+//Real-world example
+// const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt('Ingredient 2?'),
+//   prompt('Ingredient 3?'),
+// ];
+// console.log(ingredients);
+//Tedious way
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// //Faster way
+// restaurant.orderPasta(...ingredients);
+
+//Objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant.name);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
