6f61ee6f73a95d5acd745fcfec91f9b97bd8707e
najmoaden <78856004+najmoaden@users.noreply.github.com>
2022-07-18 19:32:11 +0300

    Logical Assignment Operator

--- script.js
+++ script.js
@@ -237,7 +237,7 @@ if (restaurant.orderPizza) {
 }
 restaurant.orderPizza && restaurant.orderPizza('mushroom', 'spinach');
  */
-
+/* 
 //The Nullish Coalescing Operator (??)
 
 restaurant.numGuest = 0;
@@ -246,4 +246,35 @@ console.log(guest);
 
 //Nullish: null and undefined (NOT 0 or '')
 const guestCorrect = restaurant.numGuest ?? 10;
-console.log(guestCorrect);
+console.log(guestCorrect); */
+
+//Logical Assignment Operator
+
+const rest1 = {
+  name: 'Capri',
+  numGuest: 0,
+};
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+//OR assignment operator
+// rest1.numGuest = rest1.numGuest || 10;
+// rest2.numGuest = rest2.numGuest || 10;
+
+// rest1.numGuest ||= 10;
+// rest2.numGuest ||= 10;
+
+//Nullish assignment operator
+rest1.numGuest ??= 10;
+rest2.numGuest ??= 10;
+
+//AND assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+console.log(rest1);
+console.log(rest2);
