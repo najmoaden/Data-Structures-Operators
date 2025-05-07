a35a3930193ecd97784fc6136a6ad9c2c895a574
Najmo Aden <najmoaden138@gmail.com>
2025-05-07 18:03:25 +0300

    :star: feat: logical assignment operators

--- scripts.js
+++ scripts.js
@@ -44,6 +44,40 @@ const restaurant = {
 };
 
 //// /////////////////////////////////////////////
+// Logical Assignment Operators
+
+const rest1 = {
+  name: 'Capri',
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// Nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+/*
+//// /////////////////////////////////////////////
 // The Nullish Coalescing Operator (??)
 
 restaurant.numGuests = 0;
@@ -54,7 +88,7 @@ console.log(guests1);
 const guestCorrect = restaurant.numGuests ?? 10;
 console.log(guestCorrect);
 
-/*
+
 //// /////////////////////////////////////////////
 // Short Circuiting (&& and ||)
 console.log('--- OR ---');
