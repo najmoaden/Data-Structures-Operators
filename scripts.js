dd1f77f263f2953077f22bf8c2146731afb1dbc1
Najmo Aden <najmoaden138@gmail.com>
2025-05-08 18:10:40 +0300

    :star: feat: loop objects: object keys, values and entries

--- scripts.js
+++ scripts.js
@@ -44,6 +44,33 @@ const restaurant = {
 };
 
 ///////////////////////////////////////////////////
+// Looping Objects: Object Keys, Values, and Entries
+
+// Property NAMES
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of Object.keys(openingHours)) {
+  openStr += `${day}, `;
+}
+
+console.log(openStr);
+
+// Property VALUES
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key}, we open at ${open} and close at ${close}`);
+}
+/*
+///////////////////////////////////////////////////
 // Optional Chaining (?.)
 if (restaurant.openingHours && restaurant.openingHours.mon)
   console.log(restaurant.openingHours.mon.open);
@@ -69,7 +96,7 @@ const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
 
 console.log(users[0]?.name ?? 'User array empty');
 
-/*
+
 ///////////////////////////////////////////////////
 // Looping Arrays: The for-of Loop
 const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
