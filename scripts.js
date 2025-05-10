2f0c2da3b7c7e3e2630071cc10f0fea06dbd494b
Najmo Aden <najmoaden138@gmail.com>
2025-05-10 15:51:31 +0300

    :star: feat: sets

--- scripts.js
+++ scripts.js
@@ -42,6 +42,40 @@ const restaurant = {
     console.log(mainIngredient, otherIngredients);
   },
 };
+
+////// ////////////////////////////////////////////
+// Sets
+
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+console.log(ordersSet);
+console.log(new Set('Jonas'));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+//ordersSet.clear();
+console.log(ordersSet);
+
+for (const order of ordersSet) {
+  console.log(order);
+}
+// Example
+const staff = ['Waiter', 'Chef', 'Manager', 'Waiter', 'Chef', 'Manager'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(new Set(['Waiter', 'Chef', 'Manager']).size);
+
+console.log(new Set('jonasschmedtmann').size);
+
 //////////////////////////////////////////////////
 // Coding Challenge #2
 /*
@@ -69,6 +103,7 @@ Lewandowski: 2
 }
 */
 
+/*
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
