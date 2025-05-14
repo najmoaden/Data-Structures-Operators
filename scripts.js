427aa609ff0784fb9cb5ef01f78236bc1763cb8d
Najmo Aden <najmoaden138@gmail.com>
2025-05-14 16:18:59 +0300

    :star: feat: challenge 4

--- scripts.js
+++ scripts.js
@@ -43,6 +43,56 @@ const restaurant = {
   },
 };
 
+/////////////////////////////////////////////////////
+// Coding Challenge #4
+/*
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to insert the elements), and conversion will happen when the button is pressed.
+
+Test data (pasted to textarea, including spaces):
+
+underscore_case
+first_name
+Some_Variable
+calculate_AGE
+delayed_departure
+
+Should produce this output (5 separate console.log outputs):
+
+underscoreCase ✅
+firstName ✅✅
+someVariable ✅✅✅
+calculateAge ✅✅✅✅
+delayedDeparture ✅✅✅✅✅
+
+Hints:
+Hint 1: Remember which character defines a new line in the textarea 😉
+Hint 2: The solution only needs to work for a variable made out of 2 words, like a_b
+Hint 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+Hint 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+Afterwards, test with your own test data!
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
+
+/*
 ////// //////////////////////////////////////////////
 // Working with Strings - Part 3
 console.log('a+very+nice+string'.split('+'));
@@ -93,7 +143,7 @@ planesInLine(5);
 planesInLine(3);
 planesInLine(12);
 
-/*
+
 ////// //////////////////////////////////////////////
 // Working with Strings - Part 2
 
